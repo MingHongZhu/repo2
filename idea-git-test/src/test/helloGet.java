3920aaef4029fa1a15ec1ef8519be68b3df72ebb
zhangsan <zhangsan@itheima>
2018-10-16 20:38:43 +0800

    提交了一份代码

---
+++ idea-git-test/src/test/helloGet.java
@@ -0,0 +1,10 @@
+package test;
+
+public class helloGet {
+    public static void main(String[] args) {
+        System.out.println("hello  Get");
+        System.out.println("hello  Get");
+        System.out.println("hello  Get");
+        System.out.println("hello  Get");
+    }
+}
